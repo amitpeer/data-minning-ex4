40426511590b1abb1d9bf57d7dc8f08e4bdf48c4
amitp <amitpeer05@gmail.com>
2017-06-13 16:59:27 +0300

    builder

--- Builder.py
+++ Builder.py
@@ -1,12 +1,18 @@
 import pandas as pd
 import re
-class Builder:
 
+
+class Builder:
     def __init__(self):
         print("Builder ctr")
+        self.attributes = {}
 
-
+    def build(self, path, bin):
+        self.readStructure(path)
 
     def readStructure(self, path):
-        file = open(path, 'r')
-        
+        print("Builder.readStructure")
+        with open(path, 'r') as structure:
+            for line in structure:
+                lineSplitBySpaces = line.split()
+                self.attributes[lineSplitBySpaces[1]] = lineSplitBySpaces[2]
