a3794316abb564466eb670976e720707c7faa499
windows <alonmor89@gmail.com>
2017-06-13 17:08:39 +0300

    blabal

--- Builder.py
+++ Builder.py
@@ -3,10 +3,38 @@ import re
 class Builder:
 
     def __init__(self):
+        self.attributes = {}
+
         print("Builder ctr")
 
 
 
     def readStructure(self, path):
         file = open(path, 'r')
-        
+
+
+
+
+    def proccessData(self,path,bin):
+        data = pd.read_csv(path);
+        for att in self.attributes :
+            if(self.attributes[att] == "NUMERIC") :
+                #firs fill miss data with avarage value (use fillna and mean)
+                data[att] = data.groupby("class").transform(lambda x: x.fillna(x.mean()))
+                #now Discretization
+                labels = range(bin+1)
+                colBin = pd.cut(att, bins=bin, labels=labels, include_lowest=True)
+
+
+
+             if(att == "class") :
+                #we are in categorical attribute
+                a=1
+            else :
+                a=1
+
+
+
+
+
+
