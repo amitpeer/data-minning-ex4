9f5d1a9ce3f92b38573f34a8a1c8eadc2fd3e23f
windows <alonmor89@gmail.com>
2017-06-14 14:44:54 +0300

    discritization problem

--- Builder.py
+++ Builder.py
@@ -21,6 +21,20 @@ class Builder:
                 lineSplitBySpaces = line.split()
                 self.attributes[lineSplitBySpaces[1]] = lineSplitBySpaces[2]
 
+    def discretization(self, col, bins):
+        minval = col.min()
+        maxval = col.max()
+        interval = (maxval-minval)/bins
+        break_points = [minval]
+        for i in range(0,bins):
+            break_points.insert(minval+interval)
+            minval = minval+interval
+
+        break_points.insert(maxval)
+        labels = range(len(break_points)-1)
+        colbins = pd.cut(col, bins=break_points, labels=labels, include_lowest=True)
+        return colbins
+
     # arrange attributes dictionary so all the attributes values would be in array
     def arrangeAttributes(self):
         print("arrangeAttributes")
@@ -29,23 +43,32 @@ class Builder:
                 attribute = self.attributes[att][1:-1]
                 attribute = attribute.split(',')
                 self.attributes[att] = attribute
+        for key, value in self.attributes.items():
+            print(key, value)
 
     # fill missing values + discrimination
     def proccessData(self, path, bin):
         print("proccessData")
         data = pd.read_csv(path)
-        for att in self.attributes:
+        for att in data:
             if (self.attributes[att] == "NUMERIC"):
                 # fill miss data with avarage value (use fillna and mean)
                 data[att] = data.groupby("class").transform(lambda x: x.fillna(x.mean()))
 
                 # Discretization
-                data[att] = pd.cut(data[att], bin, labels=False)
+                #data[att] = discretization(data[att], bin)
+                data[att] = pd.cut(data[att], bins=bin, labels=False)
 
                 # change "numeric" to the values after Discertization
-                self.attributes[att] = range(0,bin)
+                self.attributes[att] = range(0, bin)
 
             elif (att != "class"):
                 # categorical issue
                 data[att].fillna(data[att].mode()[0], inplace=True)
+
+        for var in data:
+            print(data[var])
         return data
+
+
+
